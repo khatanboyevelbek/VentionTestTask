f7750d0b01fed36ec4858beb10c27a6dff3c5617
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-07-15 21:01:28 +0500

    Implament Logger Service

---
+++ VentionTestTask.Application/Loggings/ILogging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentionTestTask.Application.Loggings
+{
+    public interface ILogging
+    {
+        void LogInformation(string message);
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
---
+++ VentionTestTask.Application/Loggings/Logging.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace VentionTestTask.Application.Loggings
+{
+    public class Logging : ILogging
+    {
+        private readonly ILogger<Logging> logger;
+
+        public Logging(ILogger<Logging> logger) =>
+            this.logger = logger;
+
+        public void LogCritical(Exception exception)
+        {
+            this.logger.LogCritical(exception, message: exception.Message);
+        }
+
+        public void LogError(Exception exception)
+        {
+            this.logger.LogError(exception, message: exception.Message);
+        }
+
+        public void LogInformation(string message)
+        {
+            this.logger.LogInformation(message);
+        }
+    }
+}
