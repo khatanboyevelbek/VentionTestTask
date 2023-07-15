d6323f63e7b4a733149f232672f402a839ebbfc2
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-07-15 19:56:12 +0500

    Implament  Order Repository

---
+++ VentionTestTask.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using VentionTestTask.Domain.Entities;
+using VentionTestTask.Infrastructure.Data;
+using VentionTestTask.Infrastructure.IRepositories;
+
+namespace VentionTestTask.Infrastructure.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(AppDbContext context)
+            : base(context)
+        { }
+    }
+}
