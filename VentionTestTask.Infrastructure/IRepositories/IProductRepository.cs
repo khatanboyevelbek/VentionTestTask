1184cd6c45a573152d9ff87d65630062d9df9069
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-07-15 19:59:25 +0500

    Implament Product Repository

---
+++ VentionTestTask.Infrastructure/IRepositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VentionTestTask.Domain.Entities;
+
+namespace VentionTestTask.Infrastructure.IRepositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    { }
+}
---
+++ VentionTestTask.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using VentionTestTask.Domain.Entities;
+using VentionTestTask.Infrastructure.Data;
+using VentionTestTask.Infrastructure.IRepositories;
+
+namespace VentionTestTask.Infrastructure.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context)
+            : base(context)
+        { }
+    }
+}
