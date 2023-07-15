081c86624731b5dd02b83c9d900f51fc1eb7f6d2
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-07-15 21:10:41 +0500

    Implament security password

---
+++ VentionTestTask.Application/Security/SecurityPassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt.Net;
+
+namespace VentionTestTask.Application.Security
+{
+    public class SecurityPassword : ISecurityPassword
+    {
+        public string Encrypt(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password, workFactor: 13, HashType.SHA512);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, passwordHash, HashType.SHA512);
+        }
+    }
+}
