72d19d45767370f07aa2692bebd5b389d2454352
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-07-16 15:10:11 +0500

    SERVICE: DeleteCategoryAsync()

--- VentionTestTask.Application/Services/Categories/CategoryService.cs
+++ VentionTestTask.Application/Services/Categories/CategoryService.cs
@@ -79,9 +79,48 @@ namespace VentionTestTask.Application.Services.Categories
             }
         }
 
-        public Task DeleteCategoryAsync(Guid categoryId)
+        public async Task DeleteCategoryAsync(Guid categoryId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (categoryId == Guid.Empty)
+                {
+                    throw new ArgumentException("ProductId cannot be null");
+                }
+
+                Category existingCategory = await this.categoryRepository.SelectById(categoryId);
+
+                if (existingCategory == null)
+                {
+                    throw new NotFoundExceptions("Category is not found with this Id");
+                }
+
+                await this.categoryRepository.DeleteAsync(existingCategory);
+            }
+            catch (ArgumentNullException exception)
+            {
+                this.logging.LogError(exception);
+
+                throw new DtoValidationExceptions("Failed CategoryDto validation error occured. Try again!", exception);
+            }
+            catch (NotFoundExceptions exception)
+            {
+                this.logging.LogError(exception);
+
+                throw new ItemDependencyExceptions("Category is not found. Try again!", exception);
+            }
+            catch (SqlException exception)
+            {
+                this.logging.LogCritical(exception);
+
+                throw new FailedStorageExceptions("Failed category storage error occured. Contact support!", exception);
+            }
+            catch (Exception exception)
+            {
+                this.logging.LogCritical(exception);
+
+                throw new FailedServiceExceptions("Unexpected system error occured. Contact support!", exception);
+            }
         }
 
         public IQueryable<Category> RetrieveAllCategoriesAsync()
