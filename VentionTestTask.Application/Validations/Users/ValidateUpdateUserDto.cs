daf54112077fedeab56c07150f80cd4e369933ee
Elbek Khatanboyev <programmerelbek@gmail.com>
2023-07-16 12:53:49 +0500

    Implament ValidateUpdateUserDto

---
+++ VentionTestTask.Application/Validations/Users/ValidateUpdateUserDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using VentionTestTask.Domain.DTOs;
+
+namespace VentionTestTask.Application.Validations.Users
+{
+    public class ValidateUpdateUserDto : AbstractValidator<UpdateUserDto>
+    {
+        public ValidateUpdateUserDto() 
+        {
+            RuleFor(s => s.Id).NotNull().NotEmpty()
+                .WithMessage("Please provide a valid Id");
+
+            RuleFor(s => s.Name).NotNull().NotEmpty()
+                .WithMessage("Please provide a valid name");
+
+            RuleFor(s => s.Email).NotNull().NotEmpty().EmailAddress()
+               .WithMessage("Please provide a valid email address");
+
+            RuleFor(s => s.Password).NotNull().Must(p => p.Length >= 8)
+                .WithMessage("Password should be minumum 8 characters");
+
+            RuleFor(s => s.Address).NotNull().NotEmpty()
+                .WithMessage("Please provide a valid address");
+
+            RuleFor(s => s.Phone).NotNull().NotEmpty()
+                .WithMessage("Please provide a valid phone number");
+        }
+    }
+}
